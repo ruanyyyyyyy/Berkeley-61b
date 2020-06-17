fb5533710635d7445bd9f7d99bd1a2de7f0169c1
ruanyue <ruanyuechn@163.com>
2020-06-16 23:53:03 -0700

    finish lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,13 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList l = A;
+        while ( l.rest!= null) {
+            l = l.rest;
+        }
+        l.rest = B;
+        return A;
+
     }
 
     /**
@@ -91,7 +97,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
