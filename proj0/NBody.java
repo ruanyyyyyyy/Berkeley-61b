00fbd02cd8d8580cf3a4648baf37fa9294c9d76e
ruanyue <ruanyuechn@163.com>
2020-06-20 16:50:45 -0700

    delete wrong words

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -70,7 +70,7 @@ public class NBody {
             StdDraw.pause(10);
         }
         StdOut.printf("%d\n", planets.length);
-        StdOut.printf("%.2e\n", r);:wq
+        StdOut.printf("%.2e\n", r);
         for (int i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                     planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
