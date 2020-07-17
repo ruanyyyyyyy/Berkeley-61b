c852b6ba3b3b0b95b1e1c2a21ddc29c6dd246c35
ruanyue <ruanyuechn@163.com>
2020-07-16 23:19:56 -0700

    correct TestOFfByOne

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,10 +9,13 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('A', 'b'));
         assertFalse(offByOne.equalChars('a', 'e'));
 
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
         assertTrue(offByOne.equalChars('&', '%'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('E', 'D'));
     }
 }
